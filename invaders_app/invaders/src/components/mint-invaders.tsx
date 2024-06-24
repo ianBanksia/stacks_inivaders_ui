182731a46ca28ca3c3740f1e6e31f6964047877f
Ian Banksia <123143735+ianBanksia@users.noreply.github.com>
2024-06-24 16:45:58 +1000

    fix post condition

--- invaders_app/invaders/src/components/mint-invaders.tsx
+++ invaders_app/invaders/src/components/mint-invaders.tsx
@@ -47,7 +47,7 @@ const roo_token = createAssetInfo(
       const postConditions = [ makeStandardSTXPostCondition(
         stxAddress!,
         FungibleConditionCode.LessEqual,
-        '1500000'
+        '2000000'
       ),
     ]; 
 
