e464da16cd884388063b739b2b67e6a3af1046c7
Ian Banksia <123143735+ianBanksia@users.noreply.github.com>
2024-07-14 04:40:55 +1000

    new background + price goes up (7stx)

--- invaders_app/invaders/src/components/mint-invaders.tsx
+++ invaders_app/invaders/src/components/mint-invaders.tsx
@@ -47,7 +47,7 @@ const roo_token = createAssetInfo(
       const postConditions = [ makeStandardSTXPostCondition(
         stxAddress!,
         FungibleConditionCode.LessEqual,
-        '5000000'
+        '7000000'
       ),
     ]; 
 
